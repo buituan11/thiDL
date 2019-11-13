c7a238ba5b3aa07e499ade085f59fdba25b473dc
buituan11 <buituan71099@gmail.com>
2019-11-13 22:22:15 +0700

    login 1

---
+++ ThiDieuLenh/Areas/Admin/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThiDieuLenh.Areas.Admin.Models
+{
+    public class HomeModel
+    {
+        public string NamHoc { set; get; }
+        public string SoCau { set; get; }
+    }
+}
